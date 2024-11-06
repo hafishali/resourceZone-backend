0d9e5c30e29137418da1a863176c6d81322ffbf3
hafishali <hafishali4@gmail.com>
2024-11-06 11:49:27 +0530

    nodemailer installation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,21 @@
 const userdts = require('../models/userModel'); // Ensure this is correctly referenced
+const nodemailer = require('nodemailer');
+const path = require('path');
 
+require('dotenv').config();  
+
+
+// Configure Nodemailer for Gmail
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure:true,
+    host:'smtp.gmail.com',
+    port:465,
+    auth: {
+        user: process.env.GMAIL,  // Should pull the value from .env
+        pass: process.env.APP_PASSWORD   // Replace with your Gmail app-specific password
+    }
+});
 exports.addUserData = async (req, res) => {
     try {
         const { message, job_position, email, Name } = req.body;
@@ -19,9 +35,32 @@ exports.addUserData = async (req, res) => {
 
         await newUser.save();
         const { createdAt, updatedAt, ...userWithoutTimestamps } = newUser.toObject();
-        return res.status(201).json({ message: 'Job created successfully', job: userWithoutTimestamps });
+        const mailOptions = {
+            from: `${email}`,
+            to: `${process.env.GMAIL}`,  // Replace with the recipient's email address
+            subject: `New Job Application -${Name}`,
+            text: `You have a new job application:
+                   Name: ${Name}
+                   Email: ${email}
+                   Job Position: ${job_position}
+                   Message: ${message}`,
+            attachments: [
+                {
+                    filename: path.basename(resume),
+                    path: resume
+                }
+            ]
+        };
+
+        // Send the email
+        await transporter.sendMail(mailOptions);
+
+        // Send response after both saving and emailing
+        return res.status(201).json({ message: 'Job created successfully and email sent', job: userWithoutTimestamps });
+
     } catch (error) {
-        return res.status(500).json({ error: 'Failed to create job', details: error.message });
+        console.error("Error:", error);
+        return res.status(500).json({ error: 'Failed to create job or send email', details: error.message });
     }
 };
 
