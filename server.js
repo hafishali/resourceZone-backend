084456a62f485620094e2b1ae2fd99a1a482468e
hafishali <hafishali4@gmail.com>
2024-11-21 13:20:04 +0530

    updated build file to serve front end

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const AdminJobsRoutes = require('./routes/AdminJobsRoutes.js');
 const UserJobs = require('./routes/UserJobsRoutes.js');
 require('./controllers/JobSheduler.js');
 
-
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path'); // Import path module
 
 const app = express();
 connectDB();
@@ -21,10 +21,18 @@ app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/admin-jobs', AdminJobsRoutes);
 
-app.use('/documents',express.static('./documents'))
+app.use('/documents', express.static('./documents'));
 
 // userRoutes
 app.use('/user-jobs', UserJobs);
+
+// Serve static files from the 'build' folder (combined frontend)
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Route to handle all requests and serve the frontend's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
